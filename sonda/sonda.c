712b80d50959a8249bcf1bf0f33fd6a3d1050242
Teodoro Alcantara Aquino <teodoro641@hotmail.com>
2024-12-28 18:05:06 -0400

    Add mean function, reduced measurements taking to 10 each 2 seconds

--- sonda/sonda.c
+++ sonda/sonda.c
@@ -22,8 +22,10 @@ void uart_init_and_configure();
 void uart_send_float(float value);
 float measure_distance_cm();
 float calculate_median(float *data, int n);
+float calculate_mean(float *data, int n);
 
-int main() {
+int main()
+{
     stdio_init_all();
 
     uart_init_and_configure();
@@ -33,40 +35,49 @@ int main() {
     gpio_init(ECHO_PIN);
     gpio_set_dir(ECHO_PIN, GPIO_IN);
 
-    while (true) {
+    while (true)
+    {
         float measurements[NUM_MEASUREMENTS];
 
-        for (int i = 0; i < NUM_MEASUREMENTS; i++) {
+        for (int i = 0; i < NUM_MEASUREMENTS; i++)
+        {
             measurements[i] = measure_distance_cm();
-            sleep_ms(1000); // Wait 1 seconds between measurements, so... 1 measurement each second
+            sleep_ms(200); // Wait 200 seconds between measurements, so... 10 measurements each 2 second
         }
 
         float median_distance = calculate_median(measurements, NUM_MEASUREMENTS);
+        uart_puts(UART_ID, "MEDIAN: ======================>\n");
         uart_send_float(median_distance);
-
-        sleep_ms(30000); // Wait 30 seconds before next set of measurements
+        float mean_distance = calculate_mean(measurements, NUM_MEASUREMENTS);
+        uart_puts(UART_ID, "MEAN: ======================>\n");
+        uart_send_float(mean_distance);
+        sleep_ms(3000); // Wait 3 seconds before next set of measurements
     }
 }
 
-void uart_init_and_configure() {
+void uart_init_and_configure()
+{
     uart_init(UART_ID, BAUD_RATE);
     gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
     gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
 }
 
-void uart_send_float(float value) {
+void uart_send_float(float value)
+{
     char str[20];
     sprintf(str, "%.2f\n", value);
     uart_puts(UART_ID, str);
 }
 
-void trigger_pulse() {
+void trigger_pulse()
+{
     gpio_put(TRIGER_PIN, 1);
     sleep_us(20);
     gpio_put(TRIGER_PIN, 0);
 }
 
-uint32_t measure_pulse_duration(uint32_t timeout_us) {
+uint32_t measure_pulse_duration(uint32_t timeout_us)
+{
 
     // sometimes gpio_get(ECHO_PIN) == (1|0) will miss
     // when the echo pin goes high/low the elapsed_time
@@ -76,20 +87,23 @@ uint32_t measure_pulse_duration(uint32_t timeout_us) {
     // execution
     uint32_t pulse_start, pulse_end, elapsed_time;
 
-
     // Wait for the pulse to start, with timeout
     pulse_start = time_us_32();
-    while (gpio_get(ECHO_PIN) == 0) {
+    while (gpio_get(ECHO_PIN) == 0)
+    {
         elapsed_time = time_us_32() - pulse_start;
-        if (elapsed_time > timeout_us) {
+        if (elapsed_time > timeout_us)
+        {
             return 0; // Timeout
         }
     }
 
     // Wait for the pulse to end, with timeout
-    while (gpio_get(ECHO_PIN) == 1) {
+    while (gpio_get(ECHO_PIN) == 1)
+    {
         elapsed_time = time_us_32() - pulse_start;
-        if (elapsed_time > timeout_us) {
+        if (elapsed_time > timeout_us)
+        {
             return 0; // Timeout
         }
         pulse_end = time_us_32();
@@ -98,12 +112,13 @@ uint32_t measure_pulse_duration(uint32_t timeout_us) {
     return pulse_end - pulse_start;
 }
 
-float measure_distance_cm() {
+float measure_distance_cm()
+{
     const float speed_of_sound = 34300.0f;
     trigger_pulse();
     // max time the echo pin will be on in theory is 23400 us
     // because the JSN sensor can measure up to 400 cm so:
-    //t = (400cm) / (34300 cm/s) = 0.01166180 seconds
+    // t = (400cm) / (34300 cm/s) = 0.01166180 seconds
     // but you have to multiply that by two because the sonic burst
     // goes and travels back when it colide with an object so
     // the total time is around 0.023323 seconds which is equal to
@@ -113,11 +128,15 @@ float measure_distance_cm() {
 }
 
 // Function to calculate the median of an array
-float calculate_median(float *data, int n) {
+float calculate_median(float *data, int n)
+{
     // Sort the array in ascending order
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (data[j] > data[j + 1]) {
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (data[j] > data[j + 1])
+            {
                 // Swap data[j] and data[j+1]
                 float temp = data[j];
                 data[j] = data[j + 1];
@@ -126,11 +145,27 @@ float calculate_median(float *data, int n) {
         }
     }
 
-    if (n % 2 == 0) {
+    if (n % 2 == 0)
+    {
         // If even number of elements, return average of middle two
         return (data[n / 2 - 1] + data[n / 2]) / 2.0;
-    } else {
+    }
+    else
+    {
         // If odd number of elements, return middle element
         return data[n / 2];
     }
+}
+
+// Function to calculate the mean of an array
+float calculate_mean(float *data, int n)
+{
+    int sum = 0;
+    // Sort the array in ascending order
+    for (int i = 0; i < n - 1; i++)
+    {
+        sum += data[i];
+    }
+
+    return sum / n;
 }
