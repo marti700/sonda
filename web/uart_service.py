f04578e8061b1de38aa8165c36b491001aee3da2
Teodoro Alcantara Aquino <teodoro641@hotmail.com>
2024-12-31 20:07:09 -0400

    Make uart_service use the real sensor data

--- web/uart_service.py
+++ web/uart_service.py
@@ -1,25 +1,22 @@
-# import serial
+import serial
 import paho.mqtt.client as mqtt
 from datetime import datetime
 
 # Setup serial connection
-# ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
+ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
 
 # MQTT setup
 mqtt_client = mqtt.Client()
-mqtt_client.connect("10.0.0.38", 1883, 60)  # Replace with your MQTT broker details
+mqtt_client.connect("10.0.0.38", 1883, 60)  
 
 def read_uart():
-    i = 0
     while True:
-        # data = ser.readline().decode('utf-8').strip()
-        data = i + 1  
+        data = ser.readline().decode('utf-8').strip()
         if data:
             timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             message = f"{timestamp}: {data}"
             print(f"Read: {message}")
             mqtt_client.publish("sonda/uart", message)
-        i = i + 1
 
 if __name__ == "__main__":
     mqtt_client.loop_start()
